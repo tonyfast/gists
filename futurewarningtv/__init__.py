d2b5da24932720c01ac9500256bcc4bd62cb2e60
Tony Fast <tony.fast@gmail.com>
2019-11-25 13:18:52 -0500

    Revert "add a post (#5)"
    This reverts commit 79f7963fb5c6c6b5ccf03fc778d702c73f5c7954.

--- futurewarningtv/__init__.py
+++
@@ -1,7 +0,0 @@
-def load_ipython_extension(shell):
-    with __import__('importnb').Notebook(): ...
-def unload_ipython_extension(shell):
-    with __import__('importnb').Notebook(): ...
-
-with __import__('importnb').Notebook(lazy=True):
-    from . import *
